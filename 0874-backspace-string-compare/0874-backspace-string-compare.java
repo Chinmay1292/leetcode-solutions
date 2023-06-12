2f509d52b70c57287e4ab2e8b4f38ab3ef61f79e
Chinmay Chougule <84652075+Chinmay1292@users.noreply.github.com>
2023-06-12 22:22:59 +0530

    Time: 2 ms (62.79%), Space: 40.7 MB (78.45%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> S = new Stack<>();
+        Stack<Character> T = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c=='#'){
+                if(!S.isEmpty()) S.pop();
+            }
+            else S.push(c);
+        }
+        for(char c: t.toCharArray()){
+            if(c=='#'){
+                if(!T.isEmpty()) T.pop();
+            }
+            else T.push(c);
+        }
+        return S.equals(T);
+    }
+}
