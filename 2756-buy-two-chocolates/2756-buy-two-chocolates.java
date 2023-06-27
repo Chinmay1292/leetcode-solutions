03504514d873ed896dd46def4bb15904ac33fdd8
Chinmay Chougule <84652075+Chinmay1292@users.noreply.github.com>
2023-06-27 22:20:26 +0530

    Time: 3 ms (70.72%), Space: 43.4 MB (55.43%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,18 +1,28 @@
 class Solution {
     public int buyChoco(int[] arr, int money) {
-        int n = arr.length;
-        int minMon = Integer.MAX_VALUE;
-        int diff;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                diff = arr[i] + arr[j];
-                if(diff<=money){
-                    minMon = Math.min(minMon, diff);
-                }
-            }
-        }
-        if(minMon<=money){
-            return money - minMon;
+        // int n = arr.length;
+        // int minMon = Integer.MAX_VALUE;
+        // int diff;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         diff = arr[i] + arr[j];
+        //         if(diff<=money){
+        //             minMon = Math.min(minMon, diff);
+        //         }
+        //     }
+        // }
+        // if(minMon<=money){
+        //     return money - minMon;
+        // }
+        // return money;
+        
+        // 2nd approach
+
+        Arrays.sort(arr);
+        int p1 = arr[0], p2 = arr[1];
+        int total = p1+p2;
+        if(total<=money){
+            return money-total;
         }
         return money;
     }
