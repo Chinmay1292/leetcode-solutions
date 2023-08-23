9a2e495a6ab9c81a21e59548c0a7c4e42eee94bc
Chinmay Chougule <84652075+Chinmay1292@users.noreply.github.com>
2023-08-23 09:50:27 +0530

    Time: 2 ms (13.07%), Space: 40.5 MB (82.97%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        boolean vis=false;
-        int ans=0;
-        for(int i=s.length()-1; i>=0; i--){
-            if(s.charAt(i)==' '){
-                if(vis){
-                    break;
-                }
-            }
-            else{
-                vis=true;
-                ans++;
-            }
-        }
-        return ans;
+        String[] c = s.split(" ");
+        System.out.println(c[c.length-1]);
+        return c[c.length-1].length();
     }
 }
